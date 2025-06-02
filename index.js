a3bf78d86ce94c7ac64d7bbfa096db352945cd51
futpib <futpib@gmail.com>
2025-06-02 17:11:40 +0400

    Upgrade dependencies and config format

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 /* eslint sort-keys: ["error", "asc", {"caseSensitive": false}] */
 
-module.exports = {
-	rules: {
-		'array-bracket-spacing': [ 'error', 'always' ],
+module.exports = [
+	{
+		rules: {
+			'@stylistic/array-bracket-spacing': [ 'error', 'always' ],
+			'@stylistic/comma-dangle': [ 'error', 'always-multiline' ],
+			'@stylistic/object-curly-spacing': [ 'error', 'always' ],
+			'@stylistic/operator-linebreak': [ 'error', 'before' ],
 
-		camelcase: [ 'error', {
-			properties: 'never',
-		} ],
+			camelcase: [ 'error', {
+				properties: 'never',
+			} ],
 
-		'comma-dangle': [ 'error', 'always-multiline' ],
-		'object-curly-spacing': [ 'error', 'always' ],
-		'operator-linebreak': [ 'error', 'before' ],
-
-		'unicorn/custom-error-definition': 'off',
-		'unicorn/filename-case': 'off',
-		'unicorn/no-array-reduce': 'off',
-		'unicorn/prefer-module': 'off',
+			'unicorn/custom-error-definition': 'off',
+			'unicorn/filename-case': 'off',
+			'unicorn/no-array-reduce': 'off',
+			'unicorn/prefer-module': 'off',
+		},
 	},
-};
+];
